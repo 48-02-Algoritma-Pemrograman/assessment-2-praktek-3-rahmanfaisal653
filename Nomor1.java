c53a0b6e2571788810cca363512e3e94e9fd303e
rahmanfaisal653 <rahmanfaisal653@gmail.com>
2024-12-12 13:29:59 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,27 +2,16 @@ import java.util.Scanner;
 
 public class Nomor1 {
 
-    public static void fizzbuzz(int n) {
-        for (int i = 0; i < n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("fizzbuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("buzz");
-            } else {
-                System.out.println(i);
-            }
-        }
-
+    public static double luas (double alas, double tinggi){
+        return 0.5*alas*tinggi;
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
+        int a = input.nextInt();
+        int t = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-
-        fizzbuzz(N);
+        System.out.println(luas(a,t));
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length);
