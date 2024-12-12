aebebc1c0e815e06fa80b483cf4a9e4c736b727e
rahmanfaisal653 <rahmanfaisal653@gmail.com>
2024-12-12 14:31:25 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -5,11 +5,11 @@ public class Nomor1 {
     public static void fizzbuzz(int n) {
         for (int i = 0; i < n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("fizzbuzz ");
+                System.out.println("fizzbuzz");
             } else if (i % 3 == 0) {
-                System.out.println(" fizz ");
+                System.out.println("fizz");
             } else if (i % 5 == 0) {
-                System.out.println("buzz ");
+                System.out.println("buzz");
             } else {
                 System.out.println(i);
             }
